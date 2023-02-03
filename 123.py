933808a5caa7909e7427f5271025c71373862d14
slglo1o <124046271+slglo1o@users.noreply.github.com>
2023-02-03 18:05:23 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,11 +5,10 @@ import queue
 
 
 class run_new:
-    q = queue.Queue(300)
-    buy = float(0.00250)  # 买入价格
-    num = float(500.0000)  # 买入数量
-    num_thread = 40  # 线程数量 运行次数
-    response = None
+    q = queue.Queue(10000000)
+    buy = float(0.20000)  # 买入价格
+    num = float(8.0000)  # 买入数量
+    num_thread = 35  # 线程数量 运行次数
     cookie = "lang=cn; curr_fiat=CNY; _dx_uzZo5y=1e0a7ceb5aa0c9744cecf1b4d91bdc8bd0f89026e43a4337d669956912b9de8444fd1afd; finger_print=63d873b32JSFubqpy5EOFfilqUJnRtJW3LISQKl1; _ym_d=1675129781; _ym_uid=1675129781437788748; ch=null; uid=13015332; nickname=a0098aau%40163.com; is_on=1; csrftoken=5af9f0a2a5a5ba7b7ef2222d1f3dd34447d59745c14382e5; is_portfolio_margin_account_13015332=0; is_portfolio_margin_switch_status_13015332=0; c2crisktip=1; b_notify=1; pver=a50ece15864fe15647c0610ae0d7b947; defaultP2PFiat=VND; notify_close=totp; market_title=usdt; login_notice_check=%2F; lasturl=%2Ftrade%2FVGX_USDT; AWSALB=TZLSYd7b8MahS9rSZz9m25kqB5wAtbBa2PoLM8Jd746Ul1Aa3cXRMh67BXDwpErt2joNpeIlEfHROdB1cx/ZrOi7Ku3Yyqo3dwE090Av8V1/C9TQgQMant/NEDri; AWSALBCORS=TZLSYd7b8MahS9rSZz9m25kqB5wAtbBa2PoLM8Jd746Ul1Aa3cXRMh67BXDwpErt2joNpeIlEfHROdB1cx/ZrOi7Ku3Yyqo3dwE090Av8V1/C9TQgQMant/NEDri"
     headers = {
         "Host": "www.gate.io",
@@ -31,49 +30,36 @@ class run_new:
         "Accept-Language": "zh-CN,zh;q=0.9",
         "Cookie": cookie
     }
-    data = {"is_margin": 0, "engine_type": "normal", "symbol": "jam_usdt", "fundpass": "", "captcha": "",
+    data = {"is_margin": 0, "engine_type": "normal", "symbol": "watt_usdt", "fundpass": "", "captcha": "",
             "type": "ask", "rate": buy, "vol": num, "order_type": "", "iceberg": 0, "source": "web"}
 
-    # 请求包
-    def hick_ht(self):
-        while True:
-            try:
-                self.response = requests.post("https://www.gate.io/json_svr/exchange/?u=1&c=782289", data=self.data,
-                                              headers=self.headers)
-                self.q.put(self.response.json())
-                # print(self.response.json())
-            except ValueError:
-                print("出现异常")
-            finally:
-                time.sleep(0.02)
+    def queue_uee(self):
+        for x in range(100000):
+            self.q.put(1)
 
-    # 生产线程
+    def hick_ht(self):
+        try:
+            while self.q.empty() is False:
+                time.sleep(0.31)
+                self.q.get(False, 1)
+                response = requests.post("https://www.gate.io/json_svr/exchange/?u=1&c=782289", data=self.data,
+                                         headers=self.headers)
+                print(response.json())
+        except Exception as e:
+            print("!!!!!", e, "!!!!!")
+        finally:
+            time.sleep(0.3)
+
     def start(self):
+        a = threading.Thread(target=self.queue_uee)
+        a.start()
         threads = []
-        for x in range(self.num_thread):
-            t = threading.Thread(target=self.hick_ht, daemon=True)
+        for i in range(self.num_thread):
+            time.sleep(0.3)
+            t = threading.Thread(target=self.hick_ht)
             threads.append(t)
             t.start()
-            time.sleep(0.02)
-
-    # 打印
-    def qu(self):
-        print(time.strftime('%Y-%m-%d %H:%M:%S'), self.q.get())
-        self.q.task_done()
-
-    # 打印线程
-    def start_1(self):
-        for x in range(self.num_thread):
-            t = threading.Thread(target=self.qu)
-            t.start()
-            time.sleep(0.03)
 
 
-while True:
-    time.sleep(0.1)
-    run = run_new()
-    run.start()
-    run.start_1()
-    time.sleep(0.3)
-
-# run = run_new()
+run = run_new()
+run.start()
